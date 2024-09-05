299421fadac30c2f137c652ad21c3b8462fff36b
imstefleon <imstefleon@gmail.com>
2024-09-04 23:28:50 -0700

    Treefilter.m

--- filter.m
+++ filter.m
@@ -1,85 +1,90 @@
-%Esta función es para unos datos ya importados.
-%los datos corresponden a las colisiones de PB con PB
-tabla_norm=tabla_norm;
-ColumnasExtra=csvread('vertice_plomo.csv');
-Particulas=Particulas;
-if size(Particulas)(2)<=15
-  Particulas=[Particulas,ColumnasExtra(:,3:5)];
+% This function is for some already imported data.
+% The data corresponds to Pb-Pb collisions.
+tabla_norm = tabla_norm;
+ExtraColumns = csvread('vertice_plomo.csv');
+Particles = Particles;
+if size(Particles, 2) <= 15
+  Particles = [Particles, ExtraColumns(:,3:5)];
 endif
 
-%Se crea una función que ayude a filtrar datos.
-function [Filtro] = filt(Particulas)
-  %Primero necesitamos conocer las condiciones del filtro
-  %Para este ejercicio se sabe que la distancia x,y respecto a la colisión debe
-  %ser menor a 2 mm y la distancia en Z debe ser menor a 10cm.
-  tama = size(Particulas)(1);
-  for n=1:tama
-    ax=Particulas(n,6); %Valor de origen de la subparticula en X
-    ay=Particulas(n,7); %Valor de origen de la subparticula en Y
-    az=Particulas(n,8); %Valor de origen de la subparticula en Z
-    vX=Particulas(n,14);%Coordenada en X de la colisión del Plomo.
-    vY=Particulas(n,15);%Coordenada en Y de la colisión del Plomo.
-    vZ=Particulas(n,16);%Coordenada en Z de la colisión del Plomo.
+% A function is created to help filter the data.
+function [Filter] = filt(Particles)
+  % First, we need to know the filter conditions.
+  % For this exercise, it's known that the x, y distance relative to the collision
+  % must be less than 2 mm, and the Z distance must be less than 10 cm.
+  sizeParticles = size(Particles, 1);
+  for n = 1:sizeParticles
+    ax = Particles(n, 6); % X origin value of the sub-particle
+    ay = Particles(n, 7); % Y origin value of the sub-particle
+    az = Particles(n, 8); % Z origin value of the sub-particle
+    vX = Particles(n, 14); % X coordinate of the Lead (Pb) collision
+    vY = Particles(n, 15); % Y coordinate of the Lead (Pb) collision
+    vZ = Particles(n, 16); % Z coordinate of the Lead (Pb) collision
 
-    %Se hace una resta de vectores.
-    d=[ax-vX,ay-vY];    %Calcula la distancia entre ambos vectores.
-    dz=az-vZ;           %Calcula la distancia en el eje Z.
+    % Subtract vectors.
+    d = [ax - vX, ay - vY];    % Calculate the distance between both vectors.
+    dz = az - vZ;              % Calculate the distance on the Z axis.
 
-    %Verifica que se cumpla las condiciones.
-    if norm(d)<=0.2 && abs(dz)<=10
-      Filtro(n,1)=1;
+    % Check if the conditions are met.
+    if norm(d) <= 0.2 && abs(dz) <= 10
+      Filter(n, 1) = 1;
     else
-      Filtro(n,1)=0;
+      Filter(n, 1) = 0;
     endif
-    %El segundo filtro a tener en cuenta es la columna TPC,
-    %Cuando es igual a cero, significa que los datos no son buenos.
-    if Particulas(n,10)<=0
-      Filtro(n,1)=0;    %Reemplaza el valor anterior si se cumple la condicion.
+    
+    % The second filter to consider is the TPC column.
+    % When it's equal to zero, it means the data is not good.
+    if Particles(n, 10) <= 0
+      Filter(n, 1) = 0;  % Override the previous value if the condition is met.
     endif
-    %Lo mismo ocurre para la columna ITS
-    if Particulas(n,9)<=0
-      Filtro(n,1)=0;    %Reemplaza el valor anterior si se cumple la condicion.
-    endif
-  endfor
-end
-%Se crea una función que permita sacar los datos filtrados.
-function [Filtrado] = reduccion(BD,Filtro)
-  %Se necesita conocer el tamaño.
-  tam=size(BD)(1);
-  %Se inicializa el ciclo.
-  Filtrado=[];
-  for n=1:tam
-    map=[];
-    map= Filtro(n,1)==1;
-    if map == 1
-      Filtrado=[Filtrado;BD(n,:)];
+
+    % The same applies to the ITS column.
+    if Particles(n, 9) <= 0
+      Filter(n, 1) = 0;  % Override the previous value if the condition is met.
     endif
   endfor
 end
-%Se crea una función para separar las partículas positivas de las negativas.
-function [PositivosPb,NegativosPb,NormP_Pb,NormN_Pb] = separar(BD,normal)
-  tama=size(BD)(1);
-  PositivosPb=[]; NegativosPb=[]; NormN_Pb=[]; NormP_Pb=[];
-  for n=1:tama
-    map=[];
-    map= BD(n,12)==1;
+
+% A function is created to extract the filtered data.
+function [Filtered] = reduction(DB, Filter)
+  % We need to know the size.
+  sizeDB = size(DB, 1);
+  % Initialize the loop.
+  Filtered = [];
+  for n = 1:sizeDB
+    map = [];
+    map = Filter(n, 1) == 1;
     if map == 1
-      PositivosPb = [PositivosPb;BD(n,:)];
-      NormP_Pb    = [NormP_Pb;normal(n,:)];
+      Filtered = [Filtered; DB(n, :)];
+    endif
+  endfor
+end
+
+% A function is created to separate positive and negative particles.
+function [PositivesPb, NegativesPb, NormP_Pb, NormN_Pb] = separate(DB, normData)
+  sizeDB = size(DB, 1);
+  PositivesPb = []; NegativesPb = []; NormN_Pb = []; NormP_Pb = [];
+  for n = 1:sizeDB
+    map = [];
+    map = DB(n, 12) == 1;
+    if map == 1
+      PositivesPb = [PositivesPb; DB(n, :)];
+      NormP_Pb = [NormP_Pb; normData(n, :)];
     endif
     if map == 0
-      NegativosPb = [NegativosPb;BD(n,:)];
-      NormN_Pb    = [NormN_Pb;normal(n,:)];
+      NegativesPb = [NegativesPb; DB(n, :)];
+      NormN_Pb = [NormN_Pb; normData(n, :)];
     endif
   endfor
 end
-%Llamar las funciones.
-%Se crea la columna del filtro
-FiltroPb          = filt(Particulas);
 
-%Ahora se filtran los datos para el dataset y para la tabla normalizada.
-ParticulasPbFilt  = reduccion(Particulas,FiltroPb);
-Tabla_Norm_PnFilt = reduccion(tabla_norm,FiltroPb);
+% Call the functions.
+% Create the filter column.
+FilterPb = filt(Particles);
 
-%Se separan los datos en positivos y negaticos.
-[PartiPb_pos,PartiPb_neg,NormP_Pb,NormN_Pb]=separar(ParticulasPbFilt,Tabla_Norm_PnFilt);
+% Now filter the data for both the dataset and the normalized table.
+FilteredParticlesPb = reduction(Particles, FilterPb);
+Filtered_NormTable = reduction(tabla_norm, FilterPb);
+
+% Separate the data into positive and negative particles.
+[PosParticlesPb, NegParticlesPb, NormP_Pb, NormN_Pb] = separate(FilteredParticlesPb, Filtered_NormTable);
